479c210e4982d12e667add46c67ce0952c3ed5ef
DanHazard <daniel.love.jr@gmail.com>
2020-05-18 22:09:27 -0500

    initial startup.rb work is complete

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,8 +1,47 @@
+require 'pry'
+
 class Startup
+
+  attr_writer :domain, :name
+  attr_reader :name, :founder, :domain
+
+  @@all = []
 
   def initialize(name, founder, domain)
     @name = name
     @founder = founder
     @domain = domain
+
+    @@all << self
   end
+
+  def pivot(new_domain, new_name)
+    # How to make this the only way to change the name???
+    self.domain = new_domain
+    self.name = new_name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_founder(founder_name)
+    #find the FIRST startup whose founder's name matches the name passed
+    self.all.find do |startup_instance|
+      # binding.pry
+      startup_instance.founder == founder_name
+    end
+  end
+
+  def self.domains
+    #returns an array of all the domains
+    #I tried this another way and it did not work as expected
+    domain_arr = []
+    self.all.select do |startup_instance|
+      domain_arr << startup_instance.domain
+    end
+    domain_arr
+  end
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,16 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+### STARTUP INFO ###
+
+uber = Startup.new("Uber", "Travis K", "www.uber.com")
+lyft = Startup.new("Lyft", "John Doe", "www.lyft.com")
+grubhub = Startup.new("Grubhub", "Jane Doe", "www.grubhub.com")
+spotify = Startup.new("Spotify", "Swedish Dan", "www.spotify.com")
+
+### Venture Capitalist INFO ###
+
+### Funding Round INFO ###
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
